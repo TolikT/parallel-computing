9990e2d59910f46468314f8efef4dadce0583100
TolikT <anatoliytihomirov@yahoo.com>
2017-02-19 07:02:13 -0800

    hotfix

--- lab1/run.py
+++ lab1/run.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
 	delta = int((n2 - n1) / 10)
 
 	with open('seq.csv', 'w+') as seq_file:
-		for pontr in range(n1, n2+1, delta):
+		for pntr in range(n1, n2+1, delta):
 			seq_file.write("{pntr},{result}\n".format(pntr=pntr,result=int(commands.getstatusoutput("./lab1-seq {size}".format(pntr))[1])))
 	
 	for i in range(2,12):
 		with open('par{num}.csv'.format(num=i), 'w+') as par_file:
-			for pontr in range(n1, n2+1, delta):
+			for pntr in range(n1, n2+1, delta):
 				par_file.write("{pntr},{result}\n".format(pntr=pntr,result=int(commands.getstatusoutput("./lab1-par-{num} {size}".format(num=i, size=pntr))[1])))
 
 	sys.exit(0)
