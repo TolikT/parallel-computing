2caf987ac6bb2f88ca5fbb00b8e476a707861184
TolikT <anatoliytihomirov@yahoo.com>
2017-03-26 11:00:33 -0700

    changed script

--- lab4/run.py
+++ lab4/run.py
@@ -11,14 +11,19 @@ if __name__ == '__main__':
        	file_list.append(ret[1].split(" ")[-1])
 
 	# stage run
-	n1 = 2000
-	n2 = 20000
+	n1 = 20000
+	n2 = 200000
 	delta = int((n2 - n1) / 10)
 
 	for bin_file in file_list:	
-		with open('{name}.csv'.format(name=bin_file), 'w+') as log_file:
+		with open('{name}-1.csv'.format(name=bin_file), 'w+') as log_file:
 			for pntr in range(n1, n2+1, delta):
-				log_file.write("{pntr},{result}\n".format(pntr=pntr,result=int(commands.getstatusoutput("./{name} {pntr}".format(name=bin_file, pntr=pntr))[1].split("\n")[-1])))
+				log_file.write("{pntr},{result}\n".format(pntr=pntr,result=int(commands.getstatusoutput("./{name} {pntr} 1".format(name=bin_file, pntr=pntr))[1].split("\n")[-1])))
 
+				log_file.flush()
+
+		with open('{name}-4.csv'.format(name=bin_file), 'w+') as log_file:
+			for pntr in range(n1, n2+1, delta):
+				log_file.write("{pntr},{result}\n".format(pntr=pntr,result=int(commands.getstatusoutput("./{name} {pntr} 4".format(name=bin_file, pntr=pntr))[1].split("\n")[-1])))
 				log_file.flush()
 	sys.exit(0)
